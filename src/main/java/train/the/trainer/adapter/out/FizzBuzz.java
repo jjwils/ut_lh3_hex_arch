dea33d63e645ee56028429e4df68ab64d33b8240
jjwils <mail@johnwilson.me.uk>
2024-06-02 20:55:51 +0100

    F - push out fizzbuzz to an adapter of convertor

--- src/main/java/train/the/trainer/adapter/out/FizzBuzz.java
+++ src/main/java/train/the/trainer/adapter/out/FizzBuzz.java
@@ -1,17 +1,27 @@
 package train.the.trainer.adapter.out;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import train.the.trainer.application.port.Convertor;
 
+import java.io.IOException;
+
 public class FizzBuzz implements Convertor {
-    public String convert(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else {
-            return String.valueOf(number);
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public String convert(int number)  {
+        String url =  "http://localhost:9000/fizzbuzz?number=" + number;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (IOException e) {
+            throw new RuntimeException("oh no, was this supposed to work?", e);
         }
     }
 }
--- src/main/java/train/the/trainer/application/port/Convertor.java
+++ src/main/java/train/the/trainer/application/port/Convertor.java
@@ -1,5 +1,7 @@
 package train.the.trainer.application.port;
 
+import java.io.IOException;
+
 public interface Convertor {
     String convert(int i);
 }
