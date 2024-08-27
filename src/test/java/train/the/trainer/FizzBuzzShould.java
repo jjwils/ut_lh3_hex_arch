8a8ce88ecb96b58694b402891b7ba8254fcc4c05
John Wilson <mail@johnwilson.me.uk>
2024-08-27 15:45:08 +0100

    rt - rename test

--- src/test/java/train/the/trainer/FizzBuzzShould.java
+++ src/test/java/train/the/trainer/FizzBuzzShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FizzBuzzTest {
+class FizzBuzzShould {
 
     @ParameterizedTest
     @CsvSource({
