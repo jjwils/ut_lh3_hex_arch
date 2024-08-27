1891400d558de546cb9343e8324e67d244ad5ccb
John Wilson <mail@johnwilson.me.uk>
2024-08-27 20:33:22 +0100

    Revert "T** - program shoould print numbers"
    This reverts commit a7c785ddc026ee2b6f1d72cc9e4edd8ef5e37f73.

---
+++ src/main/java/train/the/trainer/Main.java
@@ -0,0 +1,8 @@
+package train.the.trainer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/train/the/trainer/Program.java
@@ -0,0 +1,16 @@
+package train.the.trainer;
+
+public class Program {
+    private final Printer printer;
+
+    public Program(Printer printer) {
+
+        this.printer = printer;
+    }
+
+    public void printNumbers(int amountToPrint) {
+        printer.print("x");
+        printer.print("x");
+
+    }
+}
